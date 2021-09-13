6af424812466326d1a1daa769fece6904bd442bf
581515 <47955690+Y581515@users.noreply.github.com>
2021-09-13 09:59:14 +0200

    Zzz

--- MinF06 - Intro til lambda-uttrykk/src/no/hvl/lph/dat108/f06/F06a.java
+++ MinF06 - Intro til lambda-uttrykk/src/no/hvl/lph/dat108/f06/F06a.java
@@ -3,7 +3,6 @@ package no.hvl.lph.dat108.f06;
 import java.util.Arrays;
 import java.util.List;
 
-
 @FunctionalInterface
 interface Sammenligner<T> {
 	int sammenlign(T a, T b);
@@ -17,73 +16,71 @@ class FornavnSammenligner implements Sammenligner<Person> {
 }
 
 public class F06a {
-	
+
 	/** En variant av sorter som bruker a.compareTo(b) */
 	private static <T extends Comparable<T>> void sorter(List<T> liste) {
-		
-		for (int i=0; i<liste.size(); i++) {
-			for (int j=1; j<liste.size(); j++) {
-				T a = liste.get(j-1);
+		sorter(liste, (a, b) -> a.compareTo(b));
+
+//		for (int i=0; i<liste.size(); i++) {
+//			for (int j=1; j<liste.size(); j++) {
+//				T a = liste.get(j-1);
+//				T b = liste.get(j);
+//				
+//				if (a.compareTo(b) > 0) { // <--- Her
+//					
+//					liste.set(j-1, b);
+//					liste.set(j, a);
+//				}
+//			}
+//		}
+	}
+
+	/** En variant av sorter som bruker s.sammenlign(a,b) */
+	private static <T extends Comparable<T>> void sorter(List<T> liste, Sammenligner<T> s) {
+
+		for (int i = 0; i < liste.size(); i++) {
+			for (int j = 1; j < liste.size(); j++) {
+				T a = liste.get(j - 1);
 				T b = liste.get(j);
-				
-				if (a.compareTo(b) > 0) { // <--- Her
-					
-					liste.set(j-1, b);
+
+				if (s.sammenlign(a, b) > 0) { // <--- Her
+
+					liste.set(j - 1, b);
 					liste.set(j, a);
 				}
 			}
 		}
 	}
 
-	/** En variant av sorter som bruker s.sammenlign(a,b) */
-	private static <T extends Comparable<T>> void sorter(List<T> liste,
-			Sammenligner<T> s) {
-		
-		for (int i=0; i<liste.size(); i++) {
-			for (int j=1; j<liste.size(); j++) {
-				T a = liste.get(j-1);
-				T b = liste.get(j);
-				
-				if (s.sammenlign(a, b) > 0) { // <--- Her
-					
-					liste.set(j-1, b);
-					liste.set(j, a);
-				}
-			}
-		}
-	}
-	
 	public static void main(String[] args) {
-		
-		//Sortere liste av heltall. Kan implementere sort med a > b
-		List<Integer> listeAvTall = Arrays.asList(4,2,7,5,9,1);
+
+		// Sortere liste av heltall. Kan implementere sort med a > b
+		List<Integer> listeAvTall = Arrays.asList(4, 2, 7, 5, 9, 1);
 		sorter(listeAvTall);
 		System.out.println(listeAvTall);
-		
-		//Sortere liste av strenger. Må innføre typeparameter og bruke
-		//Comparable.compareTo().
+
+		// Sortere liste av strenger. Må innføre typeparameter og bruke
+		// Comparable.compareTo().
 		List<String> listeAvString = Arrays.asList("Aaa", "Xxx", "Bbb", "Kkk");
 		sorter(listeAvString);
 		System.out.println(listeAvString);
-		
-		//Sortere liste av personer. Implementere standard sortering i
-		//Person.compareTo().
-		List<Person> listeAvPersoner = Arrays.asList(
-				new Person("Anne", "Persen",40),
-				new Person("Per", "Andersen",50),
-				new Person("Knut", "Svendsen",60));
+
+		// Sortere liste av personer. Implementere standard sortering i
+		// Person.compareTo().
+		List<Person> listeAvPersoner = Arrays.asList(new Person("Anne", "Persen", 40),
+				new Person("Per", "Andersen", 50), new Person("Knut", "Svendsen", 60));
 		sorter(listeAvPersoner);
 		System.out.println(listeAvPersoner);
-		
-		//Ønsker å sortere på fornavn. Hva gjør vi?
-		//Jo, trenger en ekstra parameter i sort som sier hvordan vi
-		//vil sortere (her på fornavn).
+
+		// Ønsker å sortere på fornavn. Hva gjør vi?
+		// Jo, trenger en ekstra parameter i sort som sier hvordan vi
+		// vil sortere (her på fornavn).
 		sorter(listeAvPersoner, new FornavnSammenligner());
 		System.out.println(listeAvPersoner);
-		
-		//Ønsker å sortere på fødselsår. Hva gjør vi?
-		//Vi kan også opprette et Sammenligner-objekt direkte via
-		//en anonym klasse ...
+
+		// Ønsker å sortere på fødselsår. Hva gjør vi?
+		// Vi kan også opprette et Sammenligner-objekt direkte via
+		// en anonym klasse ...
 		sorter(listeAvPersoner, new Sammenligner<Person>() {
 			@Override
 			public int sammenlign(Person a, Person b) {
@@ -91,6 +88,6 @@ public class F06a {
 			}
 		});
 		System.out.println(listeAvPersoner);
-		
+
 	}
 }
